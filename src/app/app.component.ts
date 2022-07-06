9b0c22eb1f6aeef62a045a004cac9ce649232ef9
Sandro Roth <sandroroth@outlook.com>
2022-07-06 14:44:03 +0200

    Add optional chaining

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Demo {
+  optionalProp?: {
+    requiredProp: {
+      value: string;
+    };
+  };
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'repro-form-types-in-template';
+export class AppComponent implements OnInit {
+  test: Demo = {
+    optionalProp: {
+      requiredProp: {
+        value: '',
+      },
+    },
+  };
+
+  ngOnInit() {
+    console.log(this.test.optionalProp?.requiredProp.value);
+  }
 }
